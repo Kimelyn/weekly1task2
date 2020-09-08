18c548059a4c4305c5a4edd56fc8342538ed8f86
Kimberly <kimberly.323@hotmail.com>
2020-09-08 16:36:58 +0200

    changed from char to string answer

--- weekly1task2/weekly1task2.cpp
+++ weekly1task2/weekly1task2.cpp
@@ -6,24 +6,25 @@
 
 int age = 0;
 std::string name = "";
-char c;
+std::string answer = "";
+
 
 int main()
 {
-    std::cout << "What's your name?: ";
     std::cin.ignore();
+    std::cout << "What's your name?: ";
     std::getline(std::cin, name);
     std::cout << "and your age?: ";
     std::cin >> age;
-    std::cout << "do you drink coffee?(y/n): "; 
-    std::cin >> c;
+    std::cout << "do you drink coffee?: "; 
+    std::cin >> answer;
     std::cout << "Your name is " << name << ". You are " << age << " years old. ";
 
-    if (c == 'y') {
-        std::cout << "It seems you like coffee.\n";
+    if (answer == "Yes" || answer == "yes" || answer == "y" || answer == "Y") {
+        std::cout << "It sems you like coffee.\n";
     }
-    if (c == 'n') {
-        std::cout << "Oh, you don't like coffee\n";
+    if (answer == "No" || answer == "N" || answer == "no" || answer == "n") {
+        std::cout << "Oh, you don't like coffee.\n";
     }
     system("pause");
     return 0;
